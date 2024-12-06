eeb502dfd99b560b84e1720cfa649e003cebff5b
Tobias Mueller <to.mueller@gmail.com>
2024-12-06 13:16:01 -0800

    added session catch

--- frontend/rctool/views.py
+++ frontend/rctool/views.py
@@ -345,7 +345,12 @@ def rctool_develop_initialize(request):
             import_form = import_rc_data(request.POST, request.FILES)
             session_content = request.POST.get("session_content")
 
-            df = pd.read_json(io.StringIO(session_content))
+            try:
+                df = pd.read_json(io.StringIO(session_content))
+            except Exception as e:
+                messages.error(request, "Error: session file could not be parsed.")
+                return render(request, "rctool/rctool/import/rctool_import.html", context)
+            
 
             # preprocess data from previous session
             data_raw_lst = df["data"].values.tolist()
@@ -428,7 +433,7 @@ def rctool_develop_initialize(request):
             try:
                 field_df_raw = pd.read_json(io.StringIO(field_data_json))
             except Exception as e:
-                messages.error(request, "Error: CSV file is could not be parsed.")
+                messages.error(request, "Error: CSV file could not be parsed.")
                 return render(request, "rctool/rctool/import/rctool_import.html", context)
 
             # convert first row to lower case
