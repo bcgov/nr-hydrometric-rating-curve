68c45455301324491d79235cf170eb7fa10228e7
Derek Roberts <derek.roberts@gmail.com>
2025-07-07 14:21:34 -0700

    Try /app/nginx.conf at runtime

--- frontend/start_nginx.sh
+++ frontend/start_nginx.sh
@@ -4,8 +4,5 @@ echo "---> Creating nginx.conf ..."
 echo "BACKEND URL IS $BACKEND_URL"
 export host="\$host"
 export proxy_add_x_forwarded_for="\$proxy_add_x_forwarded_for"
-envsubst < /app/nginx.conf >  /etc/nginx/nginx.conf
-echo "---> nginx.conf created"
-cat /etc/nginx/nginx.conf
-nginx -g 'daemon off;'
-
+cat /app/nginx.conf
+nginx -c /app/nginx.conf -g 'daemon off;'
