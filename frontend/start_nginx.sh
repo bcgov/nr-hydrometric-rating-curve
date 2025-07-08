7707fbd50b25f86df09bb88212f3eef3f938aab2
Derek Roberts <derek.roberts@gmail.com>
2025-07-07 17:33:36 -0700

    Further use of /tmp/

--- frontend/start_nginx.sh
+++ frontend/start_nginx.sh
@@ -3,11 +3,11 @@
 echo "---> Creating nginx.conf from template..."
 echo "BACKEND URL IS $BACKEND_URL"
 
-# Use envsubst to create final nginx.conf from template
-envsubst '${BACKEND_URL}' < /app/nginx.conf.template > /app/nginx.conf
+# Use envsubst to create final nginx.conf from template in /tmp
+envsubst '${BACKEND_URL}' < /app/nginx.conf.template > /tmp/nginx.conf
 
 echo "---> Checking nginx.conf content:"
-cat /app/nginx.conf | grep -A5 -B5 proxy_pass
+cat /tmp/nginx.conf | grep -A5 -B5 proxy_pass
 
 echo "---> Starting nginx..."
-nginx -c /app/nginx.conf -g 'daemon off;'
+nginx -c /tmp/nginx.conf -g 'daemon off;'
