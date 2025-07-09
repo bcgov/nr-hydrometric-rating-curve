9d7de588e6e85ffb2fce6652c5c89feaba16e81a
Derek Roberts <derek.roberts@gmail.com>
2025-07-09 12:25:35 -0700

    Use periods to replace log-dropped spaces in nginx.con

--- frontend/start_nginx.sh
+++ frontend/start_nginx.sh
@@ -11,8 +11,6 @@ envsubst '${BACKEND_URL}' < /app/nginx.conf.template > ${CONF}
 # Log config
 echo
 echo "---> ${CONF}:"
-sed 's/^/nginx.conf | /' ${CONF}
-sed -e 's/^/nginx.conf | /' -e 's/^\(nginx.conf | \) */\1/' /tmp/nginx.conf | sed 's/ /·/g'
 sed 's/^/nginx.conf | /; s/ /·/g' /tmp/nginx.conf
 # Create temp dirs
 mkdir -p /tmp/nginx-proxy-temp /tmp/nginx-client-temp /tmp/nginx-fastcgi-temp /tmp/nginx-uwsgi-temp /tmp/nginx-scgi-temp
