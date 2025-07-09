6d2a03bbd5b5e8dd9cc5f1a13fd924e2db865ece
Derek Roberts <derek.roberts@gmail.com>
2025-07-09 12:25:35 -0700

    Cleanup script

--- frontend/start_nginx.sh
+++ frontend/start_nginx.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-echo "---> Creating nginx.conf from template..."
-echo "BACKEND URL IS $BACKEND_URL"
+# Config vars
+CONF="/tmp/nginx.conf"
 
-# Use envsubst to create final nginx.conf from template
-envsubst '${BACKEND_URL}' < /app/nginx.conf.template > /tmp/nginx.conf
+# Create config
+echo "---> Creating ${CONF} from template"
+echo "BACKEND_URL=$BACKEND_URL"
+envsubst '${BACKEND_URL}' < /app/nginx.conf.template > ${CONF}
 
-echo "---> nginx.conf created"
-sed 's/^/nginx.conf | /' /tmp/nginx.conf
+# Log config
+echo
+echo "---> ${CONF}:"
+sed 's/^/nginx.conf | /' ${CONF}
 
-echo "---> Starting nginx..."
+# Create temp dirs
 mkdir -p /tmp/nginx-proxy-temp /tmp/nginx-client-temp /tmp/nginx-fastcgi-temp /tmp/nginx-uwsgi-temp /tmp/nginx-scgi-temp
-nginx -c /tmp/nginx.conf -g 'daemon off;'
+
+# Startup (foreground, daemon off)
+echo
+echo "---> Starting nginx..."
+nginx -c ${CONF} -g 'daemon off;'
