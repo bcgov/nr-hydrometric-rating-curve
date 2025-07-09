b4a61364d36f7b9a2b349ff57bd816454e1c2133
Derek Roberts <derek.roberts@gmail.com>
2025-07-09 10:01:08 -0700

    Clean up log printing of nginx.conf

--- frontend/start_nginx.sh
+++ frontend/start_nginx.sh
@@ -3,17 +3,11 @@
 echo "---> Creating nginx.conf from template..."
 echo "BACKEND URL IS $BACKEND_URL"
 
-# nginx envars
-# Lines removed as they are unused.
-
 # Use envsubst to create final nginx.conf from template
 envsubst '${BACKEND_URL}' < /app/nginx.conf.template > /tmp/nginx.conf
 
 echo "---> nginx.conf created"
-cat /tmp/nginx.conf
-
-echo "---> Checking nginx.conf content:"
-grep -iEA4 "location / {" /tmp/nginx.conf
+sed 's/^/nginx.conf | /' /tmp/nginx.conf
 
 echo "---> Starting nginx..."
 nginx -c /tmp/nginx.conf -g 'daemon off;'
