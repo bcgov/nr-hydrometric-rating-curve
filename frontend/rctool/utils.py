a961f6c67667b4eda384de548100ab55309b2b06
Derek Roberts <derek.roberts@gmail.com>
2026-03-17 15:11:50 -0700

    chore(ci): allow python 3.14 (#311)

--- frontend/rctool/utils.py
+++ frontend/rctool/utils.py
@@ -3,9 +3,16 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from django.shortcuts import render
 
-from xhtml2pdf import pisa
+try:
+    from xhtml2pdf import pisa
+    XHTML2PDF_AVAILABLE = True
+except ImportError:
+    XHTML2PDF_AVAILABLE = False
+    pisa = None
 
 def render_to_pdf(template_src, context_dict={}):
+    if not XHTML2PDF_AVAILABLE:
+        raise ImportError("xhtml2pdf is required for PDF export. Install with: pip install rctool[pdf]")
     print('generating pdf from utils...')
     template = get_template(template_src)
     html  = template.render(context_dict)
